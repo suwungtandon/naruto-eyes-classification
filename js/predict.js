5da0f2d689c01800d21ac618ea303920c9ef5223
Suwung <49417240+suwungtandon@users.noreply.github.com>
2019-06-25 13:18:14 +0700

    Add files via upload

---
+++ js/predict.js
@@ -0,0 +1,120 @@
+const modelURL = 'https://bhanuchander210.github.io/naruto_eyes_classification/tfmodels/lenet/model.json';
+const IMAGE_SIZE = 28;
+var results;
+
+async function main(){
+    const imgEle = await document.getElementById('inimg');
+    const model = await tf.loadLayersModel(modelURL);
+
+    // Getting the re-sized image
+    var img = tf.browser.fromPixels(imgEle).resizeNearestNeighbor([IMAGE_SIZE, IMAGE_SIZE]).toFloat();
+    console.log('Image')
+    img.print()
+    var normalized = img.mean(2).expandDims(2).div(255.0);
+    normalized = normalized.expandDims();
+    const predicted = await model.predict(normalized).data()
+
+    // get the model's prediction results
+    results = Array.from(predicted)
+    console.log('Predictions are : ', results)
+    putChart (results)
+
+}
+
+
+initialCheck()
+clearChart()
+
+async function getMyImg(){
+    return await document.getElementById('inimg');
+}
+
+async function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#inimg').attr('src', e.target.result);
+            $('#inimgShow').attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+        const imgEle = await getMyImg();
+        imgEle.style.display = "block";
+        clearChart();
+        document.getElementById("predictBtn").disabled = false;
+    }
+}
+
+$("#inputImg").change(function(){
+        readURL(this);
+});
+
+function putChart(inData){
+
+    inData[2] = inData[2] + inData[3]
+    inData.pop()
+    inData = inData.map(function(x){ return (x * 100).toFixed(2)})
+    var data = [{
+      x: ["Sharingan", "Byakugan", "Others"],
+      y: inData,
+      type: 'bar',
+      text: inData.map(String),
+      textposition: 'auto',
+      marker:{
+        color: ['rgba(222,45,38,0.8)', 'rgba(120,120,204,1)' , 'rgba(251,213,171,1)']
+      }
+    }];
+
+    var layout = {
+        title: 'Ocular Classification Result',
+        width: 400,
+        height: 300,
+        plot_bgcolor:"#c2b999",
+        paper_bgcolor:"#c2b999",
+        yaxis: {
+        rangemode: 'nonnegative',
+        range: [0, 100]
+        },
+        font: {
+            size: 9,
+        },
+        margin: {
+            l: 50,
+            r: 50,
+            b: 50,
+            t: 80,
+            pad: 4
+        }
+    }
+
+    Plotly.newPlot('plot', data, layout);
+    document.getElementById('plot').style.display = 'block';
+}
+
+
+function initialCheck(){
+
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        console.log("success! All the File APIs are supported.")
+    } else {
+        alert('The File APIs are not fully supported in this browser.');
+    }
+
+}
+
+function clearChart(){
+    putChart([0, 0, 0, 0])
+    console.log("Chart cleared.")
+}
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-100320544-1', 'auto');
+ga('send', 'pageview', {
+  'page': 'https://bhanuchander210.github.io/naruto_eyes_classification',
+  'title': 'Naruto_Eyes_classification'
+});
---
+++ readme.md
@@ -0,0 +1,128 @@
+## Naruto Ocular Power Classifier using Neural Networks
+---
+
+<p style="align: center">
+<img src="/data/naruto_eyes_prediction-web.gif" width="50%"/>
+</p>
+
+### Getting Started :
+---
+
+Classification on Naruto character eye images with various **CNN** for analysing the Neural Network accuracy.
+
+###### Info :
+---
+
+
+|Classes| NeuralNetworks|
+|-------|---------------|
+|Sharingan|CNN1|
+|Byakugan|CNN3|
+|Sage|CNN4|
+|Others|LeNet|
+||VGG16|
+
+### How to Train model with custom data?
+---
+
+
+1. Prepare Images in directory structure
+
+```commandline
+- train
+    |_sharingan
+    |_byakugan
+    |_Others
+
+```
+
+2. Train Command:
+
+```python
+python train.py -d data/train/
+```
+
+3. Test :
+
+```python
+python test.py -m modules/LENET_naruto_eye.h5 -i data/test/sage_draw.jpeg
+```
+
+### Neural Networks :
+---
+
+I have used these neural networks. Still lot to go and include ... !
+
+#### LeNet :
+---
+
+Structure:
+
+<p style="align: center">
+<img src="/plots/LENET_schema.png" width="auto" height="500px"/>
+</p>
+
+Training result:
+
+<p style="align: center">
+<img src="/plots/LENET.png" width="50%"/>
+</p>
+
+#### CNN4 :
+---
+
+Structure:
+
+<p style="align: center">
+<img src="/plots/CNN4_schema.png" width="auto" height="500px"/>
+</p>
+
+Training result:
+
+<p style="align: center">
+<img src="/plots/CNN4.png" width="auto" height="500px"/>
+</p>
+
+
+#### CNN3 :
+---
+
+Structure:
+
+<p style="align: center">
+<img src="/plots/CNN3_schema.png" width="auto" height="500px"/>
+</p>
+
+Training result:
+
+<p style="align: center">
+<img src="/plots/CNN3.png" width="auto" height="500px"/>
+</p>
+
+#### CNN1 :
+---
+
+Structure:
+
+<p style="align: center">
+<img src="/plots/CNN1_schema.png" width="auto" height="500px"/>
+</p>
+
+Training result:
+
+<p style="align: center">
+<img src="/plots/CNN1.png" width="50%"/>
+</p>
+
+#### Thanks for the References :
+---
+
+- https://towardsdatascience.com/the-4-convolutional-neural-network-models-that-can-classify-your-fashion-images-9fe7f3e5399d
+- https://medium.com/@sidereal/cnns-architectures-lenet-alexnet-vgg-googlenet-resnet-and-more-666091488df5
+- https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3
+- https://machinelearningmastery.com/use-pre-trained-vgg-model-classify-objects-photographs/
+- https://www.pyimagesearch.com/2017/12/11/image-classification-with-keras-and-deep-learning/
+- Naruto fonts by [Cufonfonts](https://www.cufonfonts.com/font/ninja-naruto)
+- [TensorFlow.js](https://js.tensorflow.org/)
+- [Release Notes of Tensorflow.js](https://github.com/tensorflow/tfjs/releases)
+- And All open source resources ..!
